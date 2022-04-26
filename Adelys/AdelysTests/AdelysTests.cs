7142f454e58109b0e8d6a0221cc6a56eea067224
Norime <loic.gondet@hotmail.fr>
2022-04-26 12:10:34 +0200

    fix: Changement du nom du fichier cs pour correspondre à notre solution

--- Adelys/AdelysTests/AdelysTests.cs
+++ Adelys/AdelysTests/AdelysTests.cs
@@ -1,9 +1,10 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace AdelysTests
 {
     [TestClass]
-    public class UnitTest1
+    public class AdelysTests
     {
         [TestMethod]
         public void TestMethod1()
